f5b0c69028d3c4c650ee5751066ff9db392342d2
Sukianto Suteja <sukialiong@gmail.com>
2021-03-04 19:37:13 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:expenses_tracker/widgets/chart.dart';
+import 'package:expenses_tracker/widgets/transaction_list.dart';
+
+import '/models/transaction.dart';
+import '/widgets/new_transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+      theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.cyan[50],
+          fontFamily: 'Quicksand',
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              primary: Colors.indigo,
+            ),
+          ),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          appBarTheme: AppBarTheme(
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  headline6: TextStyle(
+                      fontFamily: 'OpenSans',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+          )),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // String titleInput;
+  // String amountInput;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransaction = [
+    // Transaction(
+    //   id: 't1',
+    //   title: 'New Shoes',
+    //   amount: 50.00,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Groceries',
+    //   amount: 77.40,
+    //   date: DateTime.now(),
+    // ),
+  ];
+
+  List<Transaction> get _recentTransaction {
+    return _userTransaction
+        .where(
+          (element) => element.date.isAfter(
+            DateTime.now().subtract(
+              Duration(days: 7),
+            ),
+          ),
+        )
+        .toList();
+  }
+
+  void _addNewTransaction(String title, double amount, DateTime chosenDate) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: chosenDate,
+    );
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransaction.removeWhere((transaction) => transaction.id == id);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (bCtx) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Expanse Tracker',
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Chart(_recentTransaction),
+            TransactionList(_userTransaction, _deleteTransaction),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/chart.dart
@@ -0,0 +1,61 @@
+import 'package:expenses_tracker/models/transaction.dart';
+import 'package:expenses_tracker/widgets/chart_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0;
+      recentTransaction.forEach((transaction) {
+        if (transaction.date.day == weekDay.day &&
+            transaction.date.month == weekDay.month &&
+            transaction.date.year == weekDay.year) {
+          totalSum += transaction.amount;
+        }
+      });
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    }).reversed.toList();
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(groupedTransactionValues);
+    return Card(
+      margin: EdgeInsets.all(20),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues
+              .map((transaction) => Flexible(
+                    fit: FlexFit.tight,
+                    child: ChartBar(
+                        transaction['day'],
+                        transaction['amount'],
+                        totalSpending == 0.0
+                            ? 0.0
+                            : (transaction['amount'] as double) /
+                                totalSpending),
+                  ))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
